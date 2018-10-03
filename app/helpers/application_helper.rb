c48eae5a0cd014d7c9537b7bb56fc764cc5125f3
Michał Zając <michal.zajac@gmail.com>
2018-10-04 01:44:08 +0200

    Add SimpleMDE to cocktail ingredients input

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,17 @@ module ApplicationHelper
       'danger'
     end
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis: true,
+                                       fenced_code_blocks: true,
+                                       disable_indented_code_blocks: true,
+                                       autolink: true,
+                                       tables: true,
+                                       underline: true,
+                                       highlight: true
+                                      )
+    markdown.render(text).html_safe
+  end
 end
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -21,6 +21,10 @@ Rails.application.config.content_security_policy_nonce_generator = lambda do |_|
   SecureRandom.base64(16)
 end
 
+Rails.application.config.content_security_policy do |policy|
+  policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035' if Rails.env.development?
+end
+
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
