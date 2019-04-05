6ed62ec47d9435ebfc4d9365c1e617af92091e56
Michał Zając <michal.zajac@gmail.com>
2019-04-05 14:54:54 +0200

    Add garnish to StrongParams

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -69,6 +69,6 @@ class CocktailsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cocktail_params
-      params.require(:cocktail).permit(:name, :ingredients, :glassware, :technique, :signature)
+      params.require(:cocktail).permit(:name, :ingredients, :garnish, :glassware, :technique, :signature)
     end
 end
